cac8ebcdd782938c64ce47a5f7c37ef3a3582c77
RishatMamat <reshatma@gmail.com>
2025-01-16 14:42:20 -0700

    change cons

--- src/product/product.js
+++ src/product/product.js
@@ -1,5 +1,5 @@
 class Product {
-    constructor(id, type, name, version) {
+    constructor(id, type, name, version, color) {
         this.id = id;
         this.type = type;
         this.name = name;
